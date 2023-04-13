03569603e0beae4a4bcf8cc9e0f95a0d134c6198
nsmilyanski <nikola.smilyanski@gmail.com>
2023-04-13 22:15:09 +0300

    add firstname and last name colums to the user entity

--- src/main/java/com/example/pazaruvalnikbackend/model/entities/User.java
+++ src/main/java/com/example/pazaruvalnikbackend/model/entities/User.java
@@ -3,12 +3,15 @@ package com.example.pazaruvalnikbackend.model.entities;
 import com.example.pazaruvalnikbackend.model.enums.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.List;
 
@@ -18,10 +21,19 @@ import java.util.List;
 @Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User implements UserDetails {
     @Id
     @GeneratedValue
     private int id;
+
+    @NotBlank
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NotBlank
+    @Column(name = "last_name")
+    private String lastName;
 
     @Column(name = "email", nullable = false)
     private String email;
