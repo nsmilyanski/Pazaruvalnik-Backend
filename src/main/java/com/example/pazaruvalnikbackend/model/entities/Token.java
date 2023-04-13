2dc5e2cf614a32b511fb27475526e514f07ffc3b
nsmilyanski <nikola.smilyanski@gmail.com>
2023-04-13 22:14:20 +0300

    add token entity and repository

---
+++ src/main/java/com/example/pazaruvalnikbackend/model/entities/Token.java
@@ -0,0 +1,35 @@
+package com.example.pazaruvalnikbackend.model.entities;
+
+import com.example.pazaruvalnikbackend.model.enums.TokenType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tokens")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "token")
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType;
+
+    private boolean expired;
+
+    private boolean revoked;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
