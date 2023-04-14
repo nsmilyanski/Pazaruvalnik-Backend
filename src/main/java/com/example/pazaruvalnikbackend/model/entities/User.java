119d3b65f5f03a9acad0e3fe9f8391fe3773a2ca
nsmilyanski <nikola.smilyanski@gmail.com>
2023-04-14 12:28:57 +0300

    add relation to the Token entity

--- src/main/java/com/example/pazaruvalnikbackend/model/entities/User.java
+++ src/main/java/com/example/pazaruvalnikbackend/model/entities/User.java
@@ -45,6 +45,9 @@ public class User implements UserDetails {
     @Column(name = "role")
     private Role role;
 
+    @OneToMany(mappedBy = "user")
+    private List<Token> tokens;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
