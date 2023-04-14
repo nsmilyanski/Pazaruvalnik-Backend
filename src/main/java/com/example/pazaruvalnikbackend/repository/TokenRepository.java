ed65d7661927be0b614953e68c789d5dbc8c00be
nsmilyanski <nikola.smilyanski@gmail.com>
2023-04-14 12:29:40 +0300

    adjust the query for not expired tokens

--- src/main/java/com/example/pazaruvalnikbackend/repository/TokenRepository.java
+++ src/main/java/com/example/pazaruvalnikbackend/repository/TokenRepository.java
@@ -11,9 +11,8 @@ import java.util.Optional;
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Integer> {
     @Query(value = """
-      select t from Token t inner join User u\s
-      on t.user.id = u.id\s
-      where u.id = :id and (t.expired = false or t.revoked = false)\s
+      select t from Token t inner join User u on t.user.id = u.id
+      where u.id = :id and (t.expired = false or t.revoked = false)
       """)
     List<Token> findAllValidTokenByUser(Integer id);
 
