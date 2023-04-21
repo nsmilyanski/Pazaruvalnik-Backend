d0558197a747fc92180ef404272accab612f2511
nsmilyanski <nikola.smilyanski@gmail.com>
2023-04-21 10:23:06 +0300

    fix bug with build

--- src/main/java/com/example/pazaruvalnikbackend/controller/UserController.java
+++ src/main/java/com/example/pazaruvalnikbackend/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.pazaruvalnikbackend.model.dto.UserDto;
 import com.example.pazaruvalnikbackend.model.entities.User;
 import com.example.pazaruvalnikbackend.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,14 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/users")
-
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
-
-    public UserController(UserService userService, HttpServletRequest request) {
-        this.userService = userService;
-    }
 
 
     @GetMapping
@@ -29,7 +26,7 @@ public class UserController {
         return "Success";
     }
 
-    @GetMapping
+    @GetMapping("/test")
     public String secureEndPoint() {
         return "Just a test";
     }
