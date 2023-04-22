bff3f26483adde1b876565ffbc683fcd1ee60370
Nikola Smilyanski <96414322+nsmilyanski@users.noreply.github.com>
2023-04-22 22:48:30 +0300

    make class and method public

--- src/test/java/com/example/pazaruvalnikbackend/PazaruvalnikBackEndApplicationTests.java
+++ src/test/java/com/example/pazaruvalnikbackend/PazaruvalnikBackEndApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PazaruvalnikBackEndApplicationTests {
+public class PazaruvalnikBackEndApplicationTests {
 
     @Test
-    void contextLoads() {
+   public void contextLoads() {
     }
 
 }
