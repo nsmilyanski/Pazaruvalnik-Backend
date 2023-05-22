f839e1d550789a94f7a2a8db04db34553427bfe1
nsmilyanski <nikola.smilyanski@gmail.com>
2023-05-22 09:25:12 +0300

    Add openapi config

--- src/main/java/com/example/pazaruvalnikbackend/config/SecurityConfiguration.java
+++ src/main/java/com/example/pazaruvalnikbackend/config/SecurityConfiguration.java
@@ -34,7 +34,17 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
+                .requestMatchers(         "/api/v1/auth/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs",
+                        "/v3/api-docs/**",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui/**",
+                        "/webjars/**",
+                        "/swagger-ui.html")
                 .permitAll()
                 .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
 
